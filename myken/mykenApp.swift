654f57733b4c175e50628742e651dec8d94d3fa4
junaama <0naama0@gmail.com>
2024-10-12 17:21:48 -0400

    Initial Commit

---
+++ myken/mykenApp.swift
@@ -0,0 +1,17 @@
+//
+//  mykenApp.swift
+//  myken
+//
+//  Created by macbook on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct mykenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
